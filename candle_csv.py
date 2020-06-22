0277c3c812896e91d5345aaba95ea178958c1f86
lachlanbar <66787657+lachlanbar@users.noreply.github.com>
2020-06-22 09:47:18 -0400

    Made it so that any variables entered, aside from API key,would automatically be shifted to a string in lower case, added the exchange variable to the code and added default values for all variables with the exception of API key.

--- candle_csv.py
+++ candle_csv.py
@@ -4,13 +4,17 @@ def candle_csv():
     import requests
     import csv
     import datetime
-    crypto=input("input the three letter symbol for your desired cryptocurrency-")
-    currency=input("input the three letter symbol for your desired currency-")
+    exchange=input("input the name of the exchange you want information from (default is Bitbuy)-") or "bitbuy"
+    exchange=exchange.lower()
+    crypto=input("input the three letter symbol for your desired cryptocurrency (default is BTC)-") or "btc"
+    crypto=crypto.lower()
+    currency=input("input the three letter symbol for your desired currency (default is CAD)-") or "cad"
+    currency=currency.lower()
     APIkey=input("input API key-")
-    filename=input("input requested name for CSV file-")
     endtime=datetime.datetime.now().isoformat(timespec='seconds')
     starttime=(datetime.date.today()-datetime.timedelta(days=1)).isoformat()
-    http="https://api.tokendatabase.com/v1/markets/ohlcv?ids=bitbuy_spot_{0}_{1}&start={2}&end={3}&period=12hour&key={4}".format(crypto,currency,starttime,endtime,APIkey)
+    http="https://api.tokendatabase.com/v1/markets/ohlcv?ids={5}_spot_{0}_{1}&start={2}&end={3}&period=12hour&key={4}".format(crypto,currency,starttime,endtime,APIkey,exchange)
+    filename=input("input requested name for CSV file (default is csv_candle)-") or "csv_candle"
     response=requests.get(http)
     rcandle=response.json()['results'][0]['candles']
     with open(filename,'w') as new_file:
@@ -18,9 +22,3 @@ def candle_csv():
         for i in rcandle:
             writer.writerow(i)    
             
-##add default values
-##change file name (alphanumeric values only)
-##print default value
-##ake exchange variable (default bitbuy")
-##autolowercase
-   
