0e1c1c44d730ad262a6d0d331a2cae4c8c055d7d
lachlanbar <66787657+lachlanbar@users.noreply.github.com>
2020-06-25 20:49:19 -0400

    finalised the code for the csv downloader and made all necessary adjustments to the candle csv downloader. Code in question now asks for crypto exchange, cryptocurrency and real world currency, defaulting to bitbuy, BTC and CAD respectively if no values are inputted. the function will then save all relavant information to a file who's name will either be inputted by the user or defaulted to csv__hundred_trades.

--- 100trades_csv.py
+++ 100trades_csv.py
@@ -1,18 +1,18 @@
-def candle_csv():
+def trades_csv():
     import requests
     import csv
-    exchange=input("input the name of the exchange you want information from (default is Bitbuy)-") or "bitbuy"##change defaults?
+    exchange=input("input the name of the exchange you want information from (default is Bitbuy)-") or "bitbuy"
     exchange=exchange.lower()
     crypto=input("input the three letter symbol for your desired cryptocurrency (default is BTC)-") or "btc"
     crypto=crypto.lower()
     currency=input("input the three letter symbol for your desired currency (default is CAD)-") or "cad"
     currency=currency.lower()
     APIkey=input("input API key-")
-    http="https://api.tokendatabase.com/v1/markets/trades?ids={}_spot_{}_{}&limit=100&key={}".format(market,crypto,currency,APIkey)
-    filename=input("input requested name for CSV file (default is csv_candle)-") or "csv_candle"##change default
+    http="https://api.tokendatabase.com/v1/markets/trades?ids={0}_spot_{1}_{2}&limit=100&key={3}".format(exchange,crypto,currency,APIkey)
+    filename=input("input requested name for CSV file (default is csv__hundred_trades)-") or "csv__hundred_trades"
     response=requests.get(http)
-    rtrades=response.json()['results'][0]['candles']##change
+    rtrades=response.json()['results'][0]['trades']
     with open(filename,'w') as new_file:
-        writer=csv.DictWriter(new_file,fieldnames=["time",'high', 'close', 'volume_quote', 'open', 'low', 'volume'])
-        for i in rcandle:
+        writer=csv.DictWriter(new_file,fieldnames=["id","id_on_exchange","time","price","quantity","side"])
+        for i in rtrades:
             writer.writerow(i)   
