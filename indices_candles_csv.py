7f09250d009b96bcfb8fe23b8d24275e0bff330c
lachlanbar <66787657+lachlanbar@users.noreply.github.com>
2020-06-26 19:17:30 -0400

    altered function so that it will request a start and end date and time for the candle data to be pulled from. the default for the ending date and time will be whatever the current date and time is, the default for the starting date will be 1 day before the indicated ending dtae and the starting time will be th same as the ending time

--- indices_candles_csv.py
+++ indices_candles_csv.py
@@ -4,18 +4,19 @@ def indices_candle_csv():
     import requests
     import csv
     import datetime
-    exchange=input("input the name of the exchange you want information from (default is Bitbuy)-") or "bitbuy"
-    exchange=exchange.lower()
     crypto=input("input the three letter symbol for your desired cryptocurrency (default is BTC)-") or "btc"
     crypto=crypto.lower()
     APIkey=input("input API key-")
-    endtime=datetime.datetime.now().isoformat(timespec='seconds')
-    starttime=(datetime.date.today()-datetime.timedelta(days=1)).isoformat()
-    http="https://api.tokendatabase.com/v1/indices/vwap5/ohlcv?ids={1}&end=2020-01-14T12:00:00.000&start=2020-01-10T12:00:00.000&period=12hour&key={}".format(crypto,currency,starttime,endtime,APIkey,exchange)
-    filename=input("input requested name for CSV file (default is csv_candle)-") or "csv_candle"
+    enddate=input("input the date of the stopping point for which the candles will be generated in the form yyyy-mm-dd (default is current date)-") or datetime.date.today().isoformat()
+    endtime=input("input the time on the given date of the stopping point for which the candles will be generated in the form hh-mm (default is current time)-") or datetime.datetime.now().time().isoformat(timespec='minutes')
+    startdate=input("input the date of the starting point for which the candles will be generated in the form yyyy-mm-dd (default is one day before ending date)-") or (datetime.datetime.strptime(enddate, '%Y-%m-%d')-datetime.timedelta(days=1)).isoformat()[:10]
+    starttime=input("input the time on the given date of the stopping point for which the candles will be generated in the form hh-mm (default is ending time)-") or endtime
+    http="https://api.tokendatabase.com/v1/indices/vwap5/ohlcv?ids={0}&end={5}T{2}&start={4}T{1}&period=12hour&key={3}".format(crypto,starttime,endtime,APIkey,startdate,enddate)
+    print(http)
+    filename=input("input requested name for CSV file (default is csv_indices_candle)-") or "csv_indices_candle"
     response=requests.get(http)
     rcandle=response.json()['results'][0]['candles']
     with open(filename,'w') as new_file:
-        writer=csv.DictWriter(new_file,fieldnames=["time",'high', 'close', 'volume_quote', 'open', 'low', 'volume'])
+        writer=csv.DictWriter(new_file,fieldnames=["time","open","high","low","close","volume","volume_quote"])
         for i in rcandle:
             writer.writerow(i)    
